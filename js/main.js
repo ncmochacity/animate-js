d1aa29aaa7b6f1cf2a50cee04e146e45001fbd0e
Ncmocha <nickichenbattle@gmail.com>
2017-12-18 17:30:43 -0600

    first commit of the animate jquery project

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+  //hover event handler on mouseover and mouseleave
+  $(".heading").hover(function(){
+    $(this).css('color','#ebc000');
+
+  },function(){
+    $(this).css('color','#fff');
+  });
+  $(".inner").mouseover(function(){
+    $(".inner h1").css('background-color','#ae5e9b');
+
+  });
+  $(".inner").mouseout(function(){
+    $(".inner h1").css('background-color','#009fdb');
+  });
+  $(".btn").click(function(){
+      $(".inner").find(".heading").animate({
+        height:'toggle'
+      });
+  });
+  $(".box").animate({
+    left:'300',
+    top:'0'
+  },'linear')
+  .animate({
+    top:'180'
+  },'linear')
+  .animate({
+    left:'-300'
+  },'linear')
+  .animate({
+    top:'-10'
+  },'linear')
+  .animate({
+    left:'0'
+  },'linear');
+});
